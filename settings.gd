acac6720fd8a6a8b42fa15abc1ad45c392b4611c
siloricity <83559941+siloricity@users.noreply.github.com>
2026-04-04 17:10:30 -0500

    fix fullscreen
    level 7 text cut off lmao

--- settings.gd
+++ settings.gd
@@ -1,13 +1,24 @@
 extends Control
+var wind: bool
+func _ready():
+	if DisplayServer.window_get_mode() == 3:
+		$%ButtonFSCR.button_pressed = true
+		wind = true
+	update_wind()
 func _on_button_fscr_toggled(toggled_on: bool) -> void:
 	match toggled_on:
 		true:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-			$%ButtonFSCR.text = "enabled"
+			wind = true
 		false:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-			$%ButtonFSCR.text = "disabled"
+			wind = false
+	update_wind()
 func _on_button_rset_pressed() -> void:
 	get_tree().change_scene_to_file("res://reset_menu.tscn")
 func _on_button_file_pressed() -> void:
 	OS.shell_show_in_file_manager(ProjectSettings.globalize_path("user://"),true)
+func update_wind():
+	match wind:
+		false:	$%ButtonFSCR.text = "disabled"
+		true:	$%ButtonFSCR.text = "enabled"
