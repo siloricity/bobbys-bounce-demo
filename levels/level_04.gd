9f20ec3c1eb17653af34ddbed82d607303a93c65
siloricity <83559941+siloricity@users.noreply.github.com>
2026-04-09 11:24:56 -0500

    compress lv4

--- levels/level_04.gd
+++ levels/level_04.gd
@@ -1,57 +1,21 @@
 extends Node2D
-var progress: int = 0
 func _ready():
-	$bobby.death.connect(its_okay)
+	$bobby.death.connect($Overlay/retrybutton.appear)
 	$bobby.death.connect(lmao)
-	# intro sequence animation
-	var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	tween.tween_property($welcome,"modulate:a",1,1.2)
-	tween.parallel().tween_property($welcome,"position:y",200,1.2)
-	progress = 1
-func _input(_InputEvent):
-	if Input.is_action_just_pressed("click"):
-		if progress == 1: # fade out intro sequence
-			var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-			tween.tween_property($welcome,"modulate:a",0,1.2)
-	# logic to enable timer for ghost
-		if $bobby.dead == true: pass
-		else:
-			$Timer.start()
-		if $bobby.linear_velocity != Vector2.ZERO:
-			$bobby.dead = true
-	if Input.is_action_just_released("click"):
-		$Timer.stop()
-		if $failsafe.is_stopped():
-			$failsafe.start()
-func its_okay():
-	$Camera2D/retrybutton.disabled = false
-	var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-	tween.parallel().tween_property($Camera2D/retrybutton,"modulate:a",1,1.2)
-# spawn ghosts
-func _on_timer_timeout() -> void:
-	var ghost = $bobby.ghost.instantiate()
-	ghost.position = $bobby.position
-	add_child(ghost)
 # finish box signal
 func _on_finish_body_entered(body: Node2D) -> void:
 	if body == $bobby:
 		$bobby.dead = true
-		$Camera2D.finish()
+		$Overlay.finish()
 		var dict = saveman.load_game().duplicate()
 		if dict["level04"] != 1:
 			dict["level04"] = 1
 			dict["levels_completed"] += 1
 			saveman.save_game(dict)
-## move to next level
-func next_level() -> void:
-	global.checkpoint_lv = 0
-	get_tree().change_scene_to_file("res://levels/level05.tscn")
 # move camera trigger
 func lmao():
 	if $Camera2D.position != Vector2.ZERO:
-		var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-		tween.tween_property($welcome2,"position:y",650,1)
-		tween.parallel().tween_property($welcome2,"modulate:a",1,1)
+		$textbox2.appear()
 func _on_cam_trigger_body_entered(body: Node2D) -> void:
 	if body.name == "bobby":
 		var tween = get_tree().create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
